1a0f5b558a3d9064d33660a0fac89e446174e126
Анастасия Усольцева <anasta.usoltseva@gmail.com>
2017-11-27 00:27:14 +0700

    Unit tests added.

---
+++ SalaryTests/HourlyWageTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SalaryTests
+{
+    [TestFixture]
+    public class HourlyWageTests
+    {
+        [TestCase("Алексей", "Волконский", TestName = "Hourly Wage 2 Params Constructor")]
+        [Test]
+        public void HourlyWageConstructor2Params(string firstname, string surname)
+        {
+            SalaryModel.SalaryTypes.HourlyWage staff = new SalaryModel.SalaryTypes.HourlyWage(firstname, surname);
+        }
+
+        [TestCase("Алексей", "Волконский", 1, 1, TestName = "Hourly Wage 4 Params Constructor")]
+        [Test]
+        public void HourlyWageConstructor4Params(string firstname, string surname, double hp, double hw)
+        {
+            SalaryModel.SalaryTypes.HourlyWage staff = new SalaryModel.SalaryTypes.HourlyWage(firstname, surname, hp, hw);
+        }
+
+        [TestCase("Алексей", TestName = "Hourly Wage Set Firstname")]
+        [Test]
+        public void HourlyWageFirstnameSet(string firstname)
+        {
+            SalaryModel.SalaryTypes.HourlyWage staff = new SalaryModel.SalaryTypes.HourlyWage();
+            staff.Firstname = firstname;
+            Assert.AreEqual(firstname, staff.Firstname);
+        }
+
+        [TestCase("Волконский", TestName = "Hourly Wage Set Surname")]
+        [Test]
+        public void HourlyWageSurnameSet(string surname)
+        {
+            SalaryModel.SalaryTypes.HourlyWage staff = new SalaryModel.SalaryTypes.HourlyWage();
+            staff.Surname = surname;
+            Assert.AreEqual(surname, staff.Surname);
+        }
+
+        [TestCase(10, TestName = "Hourly Wage Set HourPrice")]
+        [Test]
+        public void HourlyWageHourPriceSet(double hourprice)
+        {
+            SalaryModel.SalaryTypes.HourlyWage staff = new SalaryModel.SalaryTypes.HourlyWage();
+            staff.HourPrice = hourprice;
+            Assert.AreEqual(hourprice, staff.HourPrice);
+        }
+
+        [TestCase(10, TestName = "Hourly Wage Set HoursWorked")]
+        [Test]
+        public void HourlyWageHoursWorkedSet(double hourworked)
+        {
+            SalaryModel.SalaryTypes.HourlyWage staff = new SalaryModel.SalaryTypes.HourlyWage();
+            staff.HoursWorked = hourworked;
+            Assert.AreEqual(hourworked, staff.HoursWorked);
+        }
+
+        [TestCase(-1, TestName = "(Negative) Hourly Wage Set HourseWorked")]
+        [Test]
+        public void NegativeHourlyWageHoursWorkedSet(double hours)
+        {
+            Assert.Throws<System.ArgumentException>(() =>
+            {
+                SalaryModel.SalaryTypes.HourlyWage staff = new SalaryModel.SalaryTypes.HourlyWage();
+                staff.HoursWorked = hours;
+            });
+        }
+
+        [TestCase(-1, TestName = "(Negative) Hourly Wage Set HourPrice")]
+        [Test]
+        public void NegativeHourlyWageHourPriceSet(double hourprice)
+        {
+            Assert.Throws<System.ArgumentException>(() =>
+            {
+                SalaryModel.SalaryTypes.HourlyWage staff = new SalaryModel.SalaryTypes.HourlyWage();
+                staff.HourPrice = hourprice;
+            });
+        }
+
+        [TestCase("Алексей", TestName = "Hourly Wage Get Firstname")]
+        [Test]
+        public void HourlyWageFirstnameGet(string firstname)
+        {
+            SalaryModel.SalaryTypes.HourlyWage staff = new SalaryModel.SalaryTypes.HourlyWage("Алексей", "Волконский", 1, 2);
+            Assert.AreEqual(firstname, staff.Firstname);
+        }
+
+        [TestCase("Волконский", TestName = "Hourly Wage Get Surname")]
+        [Test]
+        public void HourlyWageSurnameGet(string surname)
+        {
+            SalaryModel.SalaryTypes.HourlyWage staff = new SalaryModel.SalaryTypes.HourlyWage("Алексей", "Волконский", 1, 2);
+            Assert.AreEqual(surname, staff.Surname);
+        }
+
+        [TestCase(1, TestName = "Hourly Wage Get HourPrice")]
+        [Test]
+        public void HourlyWageHourPriceGet(double hourprice)
+        {
+            SalaryModel.SalaryTypes.HourlyWage staff = new SalaryModel.SalaryTypes.HourlyWage("Алексей", "Волконский", 1, 2);
+            Assert.AreEqual(hourprice, staff.HourPrice);
+        }
+
+        [TestCase(2, TestName = "Hourly Wage Get HoursWorked")]
+        [Test]
+        public void HourlyWageHoursWorkedGet(double hourworked)
+        {
+            SalaryModel.SalaryTypes.HourlyWage staff = new SalaryModel.SalaryTypes.HourlyWage("Алексей", "Волконский", 1, 2);
+            Assert.AreEqual(hourworked, staff.HoursWorked);
+        }
+    }
+}
---
+++ SalaryTests/MonthlyWageTests.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SalaryTests
+{
+    [TestFixture]
+    public class MonthlyWageTests
+    {
+        [TestCase("Алексей", "Волконский", TestName = "Monthly Wage 2 Params Constructor")]
+        [Test]
+        public void MonthlyWageConstructor2Params(string firstname, string surname)
+        {
+            SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage(firstname, surname);
+        }
+
+        [TestCase("Алексей", "Волконский", 1, 1, 1, TestName = "Monthly Wage 5 Params Constructor")]
+        [Test]
+        public void MonthlyWageConstructor5Params(string firstname, string surname, double rew, double rate, double bounty)
+        {
+            SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage(firstname, surname, rew, rate, bounty);
+        }
+
+        [TestCase("Алексей", TestName = "Monthly Wage Set Firstname")]
+        [Test]
+        public void MonthlyWageFirstnameSet(string firstname)
+        {
+            SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage();
+            staff.Firstname = firstname;
+            Assert.AreEqual(firstname, staff.Firstname);
+        }
+
+        [TestCase("Волконский", TestName = "Monthly Wage Set Surname")]
+        [Test]
+        public void MonthlyWageSurnameSet(string surname)
+        {
+            SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage();
+            staff.Surname = surname;
+            Assert.AreEqual(surname, staff.Surname);
+        }
+
+        [TestCase(10, TestName = "Monthly Wage Set Bounty")]
+        [Test]
+        public void MonthlyWageBountySet(double bounty)
+        {
+            SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage();
+            staff.Bounty = bounty;
+            Assert.AreEqual(bounty, staff.Bounty);
+        }
+
+        [TestCase(10, TestName = "Monthly Wage Set Rate")]
+        [Test]
+        public void MonthlyWageRateSet(double rate)
+        {
+            SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage();
+            staff.Rate = rate;
+            Assert.AreEqual(rate, staff.Rate);
+        }
+
+        [TestCase(10, TestName = "Monthly Wage Set Reward")]
+        [Test]
+        public void MonthlyWageRewardSet(double rew)
+        {
+            SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage();
+            staff.Reward = rew;
+            Assert.AreEqual(rew, staff.Reward);
+        }
+
+        [TestCase(-1, TestName = "(Negative) Monthly Wage Set Bounty")]
+        [Test]
+        public void NegativeMonthlyWageBountySet(double bounty)
+        {
+            Assert.Throws<System.ArgumentException>(() =>
+            {
+                SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage();
+                staff.Bounty = bounty;
+            });
+        }
+
+        [TestCase(-1, TestName = "(Negative) Monthly Wage Set Rate")]
+        [Test]
+        public void NegativeMonthlyWageRateSet(double rate)
+        {
+            Assert.Throws<System.ArgumentException>(() =>
+            {
+                SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage();
+                staff.Rate = rate;
+            });
+        }
+
+        [TestCase(-1, TestName = "(Negative) Monthly Wage Set Reward")]
+        [Test]
+        public void NegativeMonthlyWageRewardSet(double rew)
+        {
+            Assert.Throws<System.ArgumentException>(() =>
+            {
+                SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage();
+                staff.Reward = rew;
+            });
+        }
+
+        [TestCase(1, TestName = "Monthly Wage Get Reward")]
+        [Test]
+        public void MonthlyWageRewardGet(double rew)
+        {
+            SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage("Алексей", "Волконский", 1, 2, 3);
+            Assert.AreEqual(rew, staff.Reward);
+        }
+
+        [TestCase(2, TestName = "Monthly Wage Get Rate")]
+        [Test]
+        public void MonthlyWageRateGet(double rate)
+        {
+            SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage("Алексей", "Волконский", 1, 2, 3);
+            Assert.AreEqual(rate, staff.Rate);
+        }
+
+        [TestCase(3, TestName = "Monthly Wage Get Bounty")]
+        [Test]
+        public void MonthlyWageBountyGet(double bounty)
+        {
+            SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage("Алексей", "Волконский", 1, 2, 3);
+            Assert.AreEqual(bounty, staff.Bounty);
+        }
+
+        [TestCase("Алексей", TestName = "Monthly Wage Get Firstname")]
+        [Test]
+        public void MonthlyWageFirstnameGet(string firstname)
+        {
+            SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage("Алексей", "Волконский", 1, 2, 3);
+            Assert.AreEqual(firstname, staff.Firstname);
+        }
+
+        [TestCase("Волконский", TestName = "Monthly Wage Get Surname")]
+        [Test]
+        public void MonthlyWageSurnameGet(string surname)
+        {
+            SalaryModel.SalaryTypes.MonthlyWage staff = new SalaryModel.SalaryTypes.MonthlyWage("Алексей", "Волконский", 1, 2, 3);
+            Assert.AreEqual(surname, staff.Surname);
+        }
+    }
+}
